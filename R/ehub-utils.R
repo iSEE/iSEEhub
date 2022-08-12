b10ffa4204401137f7ce16717532c73b3fecb399
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-08-12 12:36:02 +0100

    refactor for faster and better unit testing

--- R/ehub-utils.R
+++ R/ehub-utils.R
@@ -4,15 +4,29 @@
 #' @param x Numerical or character scalar to retrieve (if necessary) and import
 #' the resource from `ehub` into R.
 #'
-#' @return A [SingleCellExperiment()] object.
+#' @return
+#' For `.load_sce()`, a [SingleCellExperiment()] object.
 #'
 #' @import SingleCellExperiment
 #' @import ExperimentHub
-#' @importFrom SummarizedExperiment SummarizedExperiment
 #'
 #' @rdname INTERNAL_load_sce
 .load_sce <- function(ehub, x) {
     object <- ehub[[x]]
+    object <- .convert_to_sce(object)
+    object
+}
+
+#' @param object An object loaded from the Bioconductor [ExperimentHub()].
+#'
+#' @return
+#' For `.convert_to_sce()`, a [SingleCellExperiment()] object.
+#'
+#' @importFrom methods is
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#'
+#' @rdname INTERNAL_load_sce
+.convert_to_sce <- function(object) {
     if (is(object, "GRanges")) {
         object <- SummarizedExperiment(rowRanges = object)
     }
--- tests/testthat/test-ehub_utils.R
+++ tests/testthat/test-ehub_utils.R
@@ -6,3 +6,26 @@ test_that(".datasets_available works", {
     out <- iSEEhub:::.load_sce(ehub, "EH7082")
     expect_s4_class(out, "SingleCellExperiment")
 })
+
+context(".convert_to_sce")
+
+test_that(".convert_to_sce works for GRanges", {
+
+    out <- iSEEhub:::.convert_to_sce(GRanges())
+    expect_s4_class(out, "SingleCellExperiment")
+
+})
+
+test_that(".convert_to_sce works for ExpressionSet", {
+
+    out <- iSEEhub:::.convert_to_sce(ExpressionSet())
+    expect_s4_class(out, "SingleCellExperiment")
+
+})
+
+test_that(".convert_to_sce works for SummarizedExperiment", {
+
+    out <- iSEEhub:::.convert_to_sce(SummarizedExperiment())
+    expect_s4_class(out, "SingleCellExperiment")
+
+})
