d7a17151b2b29252593574388c77d27334cd4c17
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-08-12 13:25:51 +0100

    Test .create_persistent_objects

--- tests/testthat/test-landing_page.R
+++ tests/testthat/test-landing_page.R
@@ -1,6 +1,13 @@
-# library(testthat); source("test-iseehub.R")
+# library(testthat); source("test-landing_page.R")
 
-test_that("iSEEhub works on rowData", {
+test_that(".landing_page works", {
     out <- iSEEhub:::.landing_page(ehub)
     expect_type(out, "closure")
+})
+
+test_that(".create_persistent_objects works", {
+    df <- data.frame(a=1, b="a")
+
+    out <- iSEEhub:::.create_persistent_objects(df)
+    expect_identical(out$datasets_visible, df)
 })
