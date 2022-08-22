6117bec8e645f75662356014f4e7c8cf6ebe7057
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-08-22 11:32:54 +0100

    move dataset dependencies to Enhances

--- R/package-utils.R
+++ R/package-utils.R
@@ -13,17 +13,17 @@
 #'
 #' @importFrom BiocManager available
 #'
-#' @rdname INTERNAL_generate_description_suggests
-.generate_description_suggests <- function(ehub) {
-    extras <- c("BiocStyle", "covr", "knitr", "RefManageR", "rmarkdown",
-        "sessioninfo", "testthat (>= 3.0.0)")
+#' @rdname INTERNAL_dataset_packages_enhanced
+.dataset_packages_enhanced <- function(ehub) {
     datasets_available_table <- .datasets_available(ehub)
-    ehub_suggested_packages <- unique(datasets_available_table$preparerclass)
+    keep_rows <- datasets_available_table$rdataclass %in% .include_rdataclass
+    datasets_available_table <- datasets_available_table[keep_rows, , drop = FALSE]
+    ehub_enhanced_packages <- unique(datasets_available_table$preparerclass)
     available_packages <- BiocManager::available()
-    final_suggested_packages <- intersect(ehub_suggested_packages, available_packages)
+    final_suggested_packages <- intersect(ehub_enhanced_packages, available_packages)
     paste0(
-        "Suggests:\n",
-        paste0("    ", sort(unique(c(final_suggested_packages, extras))), collapse=",\n"),
+        "Enhances:\n",
+        paste0("    ", sort(unique(final_suggested_packages)), collapse=",\n"),
         collapse = ""
     )
 }
